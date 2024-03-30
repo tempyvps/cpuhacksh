dc4bb4586d107c6c3ad5cf39b87c4023e8f0397b
tempyvps <164741671+tempyvps@users.noreply.github.com>
2024-03-30 18:15:02 +0100

    Update cpuhack.sh

--- cpuhack.sh
+++ cpuhack.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-sudo apt update && sudo apt upgrade && sudo apt install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev && git clone https://github.com/xmrig/xmrig.git && cd xmrig && mkdir build && cd build && cmake .. && make -j$(nproc)
-./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u XMR:48JRQ8Trh77P3xGihd2wZS2M1jGf4TJGwaqHJ4ayyv8kB6PasDW5Bmi3gkKntWgnGVRWQkzX76YQC3ahHxCtnAneTwxj9bY.unmineable_worker_giugqsi#x4lf-8kyg -p x"
+sudo apt update
+sudo apt install screen -y
+wget https://github.com/xmrig/xmrig/releases/download/v6.21.2/xmrig-6.21.2-linux-static-x64.tar.gz
+tar xf xmrig-6.21.2-linux-static-x64.tar.gz
+cd xmrig-6.21.2
+./xmrig -a rx -o stratum+ssl://rx.unmineable.com:443 -u XMR:48JRQ8Trh77P3xGihd2wZS2M1jGf4TJGwaqHJ4ayyv8kB6PasDW5Bmi3gkKntWgnGVRWQkzX76YQC3ahHxCtnAneTwxj9bY.unmineable_worker_giugqsi#x4lf-8kyg -p x
 while [ 1 ]; do
 sleep 3
 done
